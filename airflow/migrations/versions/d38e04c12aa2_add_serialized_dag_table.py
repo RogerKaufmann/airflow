b719228e19ba09aaff6baf3661a140f72df63f57
Kaufmann Roger, INI-RLA-AND-BEN <Roger.Kaufmann2@swisscom.com>
2020-02-03 11:34:01 +0100

    fixed add_serialized_dag_table

--- airflow/migrations/versions/d38e04c12aa2_add_serialized_dag_table.py
+++ airflow/migrations/versions/d38e04c12aa2_add_serialized_dag_table.py
@@ -52,24 +52,18 @@ def upgrade():
                     sa.Column('data', json_type(), nullable=False),
                     sa.Column('last_updated', sa.DateTime(), nullable=False),
                     sa.PrimaryKeyConstraint('dag_id'))
-    op.create_index(   # pylint: disable=no-member
+    op.create_index(  # pylint: disable=no-member
         'idx_fileloc_hash', 'serialized_dag', ['fileloc_hash'])
 
     if conn.dialect.name == "mysql":
         conn.execute("SET time_zone = '+00:00'")
-        cur = conn.execute("SELECT @@explicit_defaults_for_timestamp")
-        res = cur.fetchall()
-        if res[0][0] == 0:
-            raise Exception(
-                "Global variable explicit_defaults_for_timestamp needs to be on (1) for mysql"
-            )
 
         op.alter_column(  # pylint: disable=no-member
             table_name="serialized_dag",
             column_name="last_updated",
             type_=mysql.TIMESTAMP(fsp=6),
             nullable=False,
-        )
+            server_default=sa.text('CURRENT_TIMESTAMP(6)'))
     else:
         # sqlite and mssql datetime are fine as is.  Therefore, not converting
         if conn.dialect.name in ("sqlite", "mssql"):
@@ -89,4 +83,4 @@ def upgrade():
 
 def downgrade():
     """Downgrade version."""
-    op.drop_table('serialized_dag')   # pylint: disable=no-member
+    op.drop_table('serialized_dag')  # pylint: disable=no-member
